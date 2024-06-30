e62c37972138bab26d2b2725dc88b797be56b861
Z-ajamy <abdorahman0283@gmail.com>
2024-07-01 01:43:59 +0300

    class Square that defines a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+'''Define a class Square.'''
+
+
+class Square:
+    """ Represent a square. """
+    def __init__(self, size=0):
+        '''
+        Initialize a new Square.
+
+
+        Args:
+            size (int):the size of the Square must be greater or equal zero
+        '''
+        if isinstance(size, int) and size >= 0:
+            self.size = size
+        else:
+            raise TabError
