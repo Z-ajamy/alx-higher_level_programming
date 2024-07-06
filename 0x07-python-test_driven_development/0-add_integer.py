a10284c6ebd5f5ea6b80cf8b1c7fea8712de677b
Z-ajamy <abdorahman0283@gmail.com>
2024-07-06 19:19:48 +0300

    a function that adds 2 integers.

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,46 @@
+"""
+    Add module
+"""
+def add_integer(a, b=98):
+    """
+        Add functhion that add two numbers and return the result.
+
+        Args:
+            a (int or float) :the first number
+            b (int or float) :the second number
+        
+        Returns:
+            int : the product of a + b
+        
+        Can hendel integral and float numbers, but it tearn the float to integral.
+            >>> add_integer(1, 2)
+            3
+            >>> add_integer(0, 0)
+            0
+            >>> add_integer(-1, 2)
+            1
+            >>> add_integer(-1, -1)
+            2
+            >>> add_integer(1, 4645452434454324231)
+            4645452434454324232
+            >>> add_integer(5.8, 1.2)
+            6
+        
+        
+        It make type error if a or b was not integral or float.
+        >>> add_integer("1", 2)
+        Traceback(most recent call last):
+            ...
+        ValueError: a must be an integer
+        >>> add_integer(1, "2")
+        Traceback(most recent call last):
+            ...
+        ValueError: b must be an integer
+    """
+    if not isinstance(a, (int, float)):
+        raise ValueError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise ValueError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
