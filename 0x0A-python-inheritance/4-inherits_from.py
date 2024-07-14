cc33496ab8584e543e9eaec01915c69d8ae5cf61
Z-ajamy <abdorahman0283@gmail.com>
2024-07-14 21:56:54 +0300

    an empty class BaseGeometry.

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,21 +2,26 @@
 """
 Module: inherits_from
 
-This module defines a function to check if an object is an instance of a class that inherited (directly or indirectly) from the specified class.
+This module defines a function to check if an object is an instance
+of a class that inherited (directly or indirectly) from
+the specified class.
 
 Functions:
-    inherits_from(obj, a_class): Checks if obj is an instance of a class that inherited from a_class.
+    inherits_from(obj, a_class): Checks if obj is an instance
+    of a class that inherited from a_class.
 """
 
 def inherits_from(obj, a_class):
-    """Checks if an object is an instance of a class that inherited (directly or indirectly) from the specified class.
+    """Checks if an object is an instance of a class that
+    inherited (directly or indirectly) from the specified class.
 
     Args:
         obj: The object to check.
         a_class: The class to match the inheritance of obj against.
 
     Returns:
-        bool: True if obj is an instance of a subclass of a_class, False otherwise.
+        bool: True if obj is an instance of a subclass of
+        a_class, False otherwise.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module: base_geometry
+
+This module defines a BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """Represents base geometry."""
+    pass
