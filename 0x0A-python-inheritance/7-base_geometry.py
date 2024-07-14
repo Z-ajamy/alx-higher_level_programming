392471043d1f9717c763729aa29de4600c2ab5c8
Z-ajamy <abdorahman0283@gmail.com>
2024-07-15 01:50:12 +0300

    ;)

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -29,7 +29,13 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is not greater than 0.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/7-base_geometry.txt")
