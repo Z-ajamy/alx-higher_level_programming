917b7a226433d459fd0b1c2730ddbbcf99932c3a
Z-ajamy <abdorahman0283@gmail.com>
2024-07-15 13:13:57 +0300

    a class Square that inherits from Rectangle

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -25,4 +25,21 @@ class Square(Rectangle):
             ValueError: If size is less than or equal to 0.
         """
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """Returns a string representation of the square.
+
+        Returns:
+            str: A string depicting the square.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
+    
+    def area(self):
+        """Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
