658a239e937e4a4e8d176b90910d3b1fad44387a
Z-ajamy <abdorahman0283@gmail.com>
2024-07-15 23:19:13 +0300

    a function that reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module: file_reader
+
+This module defines a function to read the contents of a file.
+"""
+
+def read_file(filename=""):
+    """Reads the contents of a file and prints it to the console.
+
+    Args:
+        filename (str): The name of the file to read from. Defaults to an empty string.
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read())
