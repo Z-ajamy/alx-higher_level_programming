066896cf9dd58e76b2a716c1fc619b47f18190a6
Z-ajamy <abdorahman0283@gmail.com>
2024-07-17 03:34:10 +0300

    Update #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -144,8 +144,7 @@ class Rectangle(Base):
 
     @staticmethod
     def __the_shape(height, width):
-        """Static method to create a string representation of a rectangle
-            shape.
+        """Static method to create a string representation of a rectangle shape.
 
         Args:
             height (int): The height of the rectangle.
@@ -188,31 +187,25 @@ class Rectangle(Base):
                     self.width,
                     self.height)
 
-    def update(self, *args):
-        """Updates the attributes of the Rectangle object based on positional
-        arguments.
+    def update(self, *args, **kwargs):
+        """Updates the attributes of the Rectangle object.
 
         Args:
             *args: Variable length argument list.
                 - If one argument is passed, it updates the id attribute.
-                - If two arguments are passed, it updates the id and width
-                  attributes.
-                - If three arguments are passed, it updates the id, width, and
-                  height attributes.
-                - If four arguments are passed, it updates the id, width,
-                    height,
-                  and x attributes.
-                - If five arguments are passed, it updates all attributes:
-                  id, width, height, x, and y.
+                - If two arguments are passed, it updates the id and width attributes.
+                - If three arguments are passed, it updates the id, width, and height attributes.
+                - If four arguments are passed, it updates the id, width, height, and x attributes.
+                - If five arguments are passed, it updates all attributes: id, width, height, x, and y.
+            **kwargs: Arbitrary keyword arguments to update specific attributes.
         """
-        if args is not None:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.width = args[1]
-            if len(args) >= 3:
-                self.height = args[2]
-            if len(args) >= 4:
-                self.x = args[3]
-            if len(args) >= 5:
-                self.y = args[4]
+        if args:
+            attributes = ['id', 'width', 'height', 'x', 'y']
+            for i, value in enumerate(args):
+                if i < len(attributes):
+                    setattr(self, attributes[i], value)
+        
+        if kwargs:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
