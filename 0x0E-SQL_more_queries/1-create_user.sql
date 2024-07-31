53d2f02a5bd71867a432773bec4ee16b34fc53f5
Z-ajamy <abdorahman0283@gmail.com>
2024-08-01 02:07:02 +0300

    0

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,8 @@
--- Create the user 'user_0d_2' connecting from 'localhost' with the specified password
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Create the user if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges on all databases and tables to the user 'user_0d_2' connecting from 'localhost'
--- and allow the user to grant these privileges to other users
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+-- Grant all privileges on all databases to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
--- Reload the grant tables to ensure that the changes take effect immediately
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
