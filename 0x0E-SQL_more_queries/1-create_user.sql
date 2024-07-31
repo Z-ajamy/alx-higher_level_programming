c21e233fdef41b16a12c6171569ea14e2f907d98
Z-ajamy <abdorahman0283@gmail.com>
2024-08-01 02:04:48 +0300

    0

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,23 +2,23 @@
 -- The GRANT statement is used to grant privileges to a MySQL user.
 -- The FLUSH PRIVILEGES statement is used to reload the grant tables to ensure that the changes take effect immediately.
 
--- Create the user 'user_0d_2' connecting from 'localhost' with the specified password
+-- Create the user user_0d_2 connecting from localhost with the specified password
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Explanation:
 -- 1. CREATE USER IF NOT EXISTS: Creates a new user if the user does not already exist.
--- 2. 'user_0d_2'@'localhost': Specifies the username and host from which the user can connect.
--- 3. IDENTIFIED BY 'user_0d_1_pwd': Sets the password for the user.
+-- 2. user_0d_2@localhost: Specifies the username and host from which the user can connect.
+-- 3. IDENTIFIED BY user_0d_1_pwd: Sets the password for the user.
 -- This command is useful for creating a new user account with a specified username, host, and password.
 
--- Grant all privileges on all databases and tables to the user 'user_0d_2' connecting from 'localhost'
+-- Grant all privileges on all databases and tables to the user user_0d_2 connecting from localhost
 -- and allow the user to grant these privileges to other users
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
 
 -- Explanation:
 -- 1. GRANT ALL PRIVILEGES: Grants all available privileges.
 -- 2. ON *.*: Specifies that the privileges apply to all databases and tables.
--- 3. TO 'user_0d_2'@'localhost': Specifies the user to whom the privileges are granted.
+-- 3. TO user_0d_2@localhost: Specifies the user to whom the privileges are granted.
 -- 4. WITH GRANT OPTION: Allows the user to grant these privileges to other users.
 -- This command is useful for granting a user full access to all databases and tables and enabling them to grant these privileges to other users.
 
