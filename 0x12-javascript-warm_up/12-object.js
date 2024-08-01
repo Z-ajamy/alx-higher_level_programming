20188ad4e6061c282437418179f6769b376215b3
Z-ajamy <abdorahman0283@gmail.com>
2024-08-02 02:12:07 +0300

    0

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  
+  console.log(myObject);
+  
+  // Update the value property of myObject
+  myObject.value = 89;
+  
+  console.log(myObject);
+  
