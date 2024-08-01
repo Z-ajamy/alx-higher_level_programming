996c3e0c9fd9e4d2e1dccd419a24435bbe65401d
Z-ajamy <abdorahman0283@gmail.com>
2024-08-02 02:13:17 +0300

    0

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Define the add function
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Export the add function so it is visible from outside
+  module.exports = {
+    add
+  };
+  
