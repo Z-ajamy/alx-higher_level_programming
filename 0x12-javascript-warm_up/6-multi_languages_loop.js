52a9cea8682bd21cd1da11ad1a634cf2a1473fef
Z-ajamy <abdorahman0283@gmail.com>
2024-08-02 02:06:56 +0300

    0

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const messages = [
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing'
+  ];
+  
+  for (const message of messages) {
+    console.log(message);
+  }
+  
