b2d1541b58bed9f5f643da3517342e61108ad175
Z-ajamy <abdorahman0283@gmail.com>
2024-08-02 16:06:49 +0300

    0

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""
+ script that fetches https://alx-intranet.hbtn.io/status
+"""
 import requests
-
-
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.content.decode('utf-8'))
