cf0090d17bc1a62c571ae7d1592be4f8ea926f55
Z-ajamy <abdorahman0283@gmail.com>
2024-09-10 02:31:44 +0300

    0

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let counter_of_logMe = 0;
 exports.logMe = function (item) {
-  console.log(`${counter_of_logMe} : ${item}`);
+  console.log(`${counter_of_logMe}: ${item}`);
   counter_of_logMe++;
 }
