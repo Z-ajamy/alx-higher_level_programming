ef1879f550e6f126f1f1e098c4e137dee728be96
Z-ajamy <abdorahman0283@gmail.com>
2025-08-01 12:54:40 +0300

    if else in one line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) >= 97 and ord(i) <= 122:
-            print("{}".format(i - 32))
-        else:
-            print("{}".format(i))
+        print("{}".format(
+            chr(ord(i) - 32) if ord(i) >= 97 
+            and ord(i) <= 122 
+            else i), end="")
+    print()
