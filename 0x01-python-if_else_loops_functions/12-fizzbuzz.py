32200b1e01be7a4fdb65c37d65391d4291e992c6
Z-ajamy <abdorahman0283@gmail.com>
2025-08-01 15:20:46 +0300

    FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    flag = 0
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz ", end= "")
+            continue
+        elif i % 3 == 0:
+            print("Fizz ", end= "")
+            continue
+        elif i % 5 == 0:
+            print("Buzz ", end= "")
+            continue
+        print(i, end=" ")
+
