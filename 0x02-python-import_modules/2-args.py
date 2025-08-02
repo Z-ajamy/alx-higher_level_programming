9713a8c4e1781699f3dba26f676a501c0bdb6ca6
Z-ajamy <abdorahman0283@gmail.com>
2025-08-02 20:38:36 +0300

    from sys import argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    num = len(argv) - 1
+    if num == 0:
+        print("0 arguments.")
+    elif num == 0:
+        print("1 argument:")
+    else:
+        print("{} argument:".format(num))
+    for i in range(1, num + 1):
+        print("{}: {}".format(i, argv[i]))
