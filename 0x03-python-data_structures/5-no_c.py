b313243eb77c0a2791b3f844ad8ff94492670516
Z-ajamy <abdorahman0283@gmail.com>
2025-08-03 22:07:06 +0300

    I love you C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string:
+        str = ""
+        for i in my_string:
+            if i == "C" or i == "C":
+                continue
+            str = str + i
+        return str
+    return None
