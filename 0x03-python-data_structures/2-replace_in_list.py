365ac6e541afa839e73403c27634c2e11714b17c
Z-ajamy <abdorahman0283@gmail.com>
2025-08-03 21:18:34 +0300

    return my_list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,4 +3,4 @@ def replace_in_list(my_list, idx, element):
     if my_list and idx >= 0 and len(my_list) > idx:
         my_list[idx] = element
         return my_list
-    return None
+    return my_list
