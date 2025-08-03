199a402e793b65c0335d4fa17d4251dfabae9e88
Z-ajamy <abdorahman0283@gmail.com>
2025-08-03 21:16:03 +0300

    my_list, idx, element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if my_list and idx >= 0 and len(my_list) > idx:
+        my_list[idx] = element
+        return my_list
+    return None
