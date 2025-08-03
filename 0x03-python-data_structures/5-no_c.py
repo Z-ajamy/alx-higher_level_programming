3adcc57ce40caa94d601a93aff7f965945766907
Z-ajamy <abdorahman0283@gmail.com>
2025-08-03 22:16:06 +0300

    str1 = ""

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        str1 = ""
-        for i in my_string:
-            if i == "C" or i == "c":
-                continue
-            str1 = str1 + i
-        return str1
-    return None
+    str1 = ""
+    for i in my_string:
+        if i == "C" or i == "c":
+            continue
+        str1 = str1 + i
+    return str1
