59f510b86efd2fdcbe4437883e3d4dd7001294eb
Abd_alrahman Ahmed Mohammed Al_ajamy <140712913+Z-ajamy@users.noreply.github.com>
2025-08-03 08:59:45 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     num = len(argv) - 1
     if num == 0:
         print("0 arguments.")
-    elif num == 0:
+    elif num == 1:
         print("1 argument:")
     else:
-        print("{} argument:".format(num))
+        print("{} arguments:".format(num))
     for i in range(1, num + 1):
         print("{}: {}".format(i, argv[i]))
