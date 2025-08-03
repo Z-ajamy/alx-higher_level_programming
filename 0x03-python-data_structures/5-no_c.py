c08afcfea1eaae1b3036a03c27f9172cf139b45d
Z-ajamy <abdorahman0283@gmail.com>
2025-08-03 22:13:36 +0300

    I love you C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        str = ""
+        str1 = ""
         for i in my_string:
             if i == "C" or i == "c":
                 continue
-            str = str + i
-        return str
+            str1 = str1 + i
+        return str1
     return None
