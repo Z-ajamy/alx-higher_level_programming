2fed01abb82ff369cf0252c38796ffd08c66a8c7
Abd_alrahman Ahmed Mohammed Al_ajamy <140712913+Z-ajamy@users.noreply.github.com>
2025-08-04 12:26:59 +0000

    new_list = []

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = []
     if my_list:
-        new_list = []
         for i, j in enumerate(my_list):
             if j == search:
                 new_list.append(replace)
             else:
                 new_list.append(my_list[i])
-        return new_list
-    
+    return new_list
