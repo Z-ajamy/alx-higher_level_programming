5d6a923c0d34afb0ebdd581f62d0614dc4cb06d1
Abd_alrahman Ahmed Mohammed Al_ajamy <140712913+Z-ajamy@users.noreply.github.com>
2025-08-04 12:23:39 +0000

    search_replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,4 +7,5 @@ def search_replace(my_list, search, replace):
                 new_list.append(replace)
             else:
                 new_list.append(my_list[i])
-    return new_list
+        return new_list
+    
