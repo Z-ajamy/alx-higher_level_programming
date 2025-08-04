aef65cf5a66dad243e668d36aa11184bb1bea64f
Abd_alrahman Ahmed Mohammed Al_ajamy <140712913+Z-ajamy@users.noreply.github.com>
2025-08-04 11:34:27 +0000

    del

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list and idx < len(my_list):
+    if my_list and idx < len(my_list) and idx >= 0:
         del my_list[idx]
+    return my_list
