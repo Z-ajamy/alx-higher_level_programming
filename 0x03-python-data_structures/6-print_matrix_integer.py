2b03cbdd5ebdeee9fcb91d9e84914e36e26ebd9b
Abd_alrahman Ahmed Mohammed Al_ajamy <140712913+Z-ajamy@users.noreply.github.com>
2025-08-04 10:44:12 +0000

    2D matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,11 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in matrix:
-            for n in i:
-                print("{:d}".format(n), end=" ")
+            length = len(i)
+            for n in range(length):
+                if n < length - 1:
+                    print("{:d}".format(i[n]), end=" ")
+                else:
+                    print("{:d}".format(i[n]), end="")
+
             print()
