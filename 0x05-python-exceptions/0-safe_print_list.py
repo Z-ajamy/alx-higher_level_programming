300942e9ee0bff4cfefd50668dbc8a482fe0fac5
Abd_alrahman Ahmed Mohammed Al_ajamy <140712913+Z-ajamy@users.noreply.github.com>
2025-08-05 13:56:27 +0000

    try except finally return

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[n]), end="")
             i = i + 1
         print()
-        return i
     except IndexError:
         print()
+    finally:
         return i
