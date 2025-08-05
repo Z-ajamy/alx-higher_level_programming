add523dda6b8d16d3c388b952af1b38e0c52cb3b
Abd_alrahman Ahmed Mohammed Al_ajamy <140712913+Z-ajamy@users.noreply.github.com>
2025-08-05 14:21:44 +0000

    (ValueError, TypeError)

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -2,7 +2,7 @@
 safe_print_list_integers = \
     __import__('2-safe_print_list_integers').safe_print_list_integers
 
-my_list = [1, 2, 3, 4]
-x = len(my_list) + 4
+my_list = ["Holberton", 1, 2, "H", 3, 4, [1, 2, 3,4]]
+x = len(my_list)
 nb_print = safe_print_list_integers(my_list, x)
 print("{:d}".format(nb_print))
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[n]), end="")
             i = i + 1
-        except ValueError:
+        except (ValueError, TypeError):
             continue
         except IndexError:
             break
