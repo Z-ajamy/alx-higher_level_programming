79c350f38e629c926d751dbba0a51fc40e777b53
Z-ajamy <abdorahman0283@gmail.com>
2025-08-07 22:24:01 +0300

    dir(obj)

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    num = len(argv)
-    res = 0
-    for i in range(1, num):
-        res += int(argv[i])
-    print(res)
--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python3
-"""Defines an object attribute lookup function."""
+"""
+Module for object attribute inspection utilities.
+
+This module provides utilities for inspecting Python objects and retrieving
+information about their attributes and methods.
+"""
 
 
 def lookup(obj):
-    """Returns a list of available attributes and methods of an object.
-
+    """
+    Get a list of valid attributes for an object.
+    
+    This function returns a list of all valid attributes and methods
+    available for the given object, including both built-in and
+    user-defined attributes.
+    
     Args:
-        obj: The object to inspect.
-
+        obj: Any Python object to inspect.
+    
     Returns:
-        list: A list of strings representing the attributes and methods
-        available on the object.
+        list: A list of strings representing all valid attribute names
+              for the given object, sorted alphabetically.
+    
+    Example:
+        >>> lookup([1, 2, 3])
+        ['append', 'clear', 'copy', 'count', 'extend', ...]
+        
+        >>> lookup("hello")
+        ['capitalize', 'casefold', 'center', 'count', 'encode', ...]
     """
     return dir(obj)
