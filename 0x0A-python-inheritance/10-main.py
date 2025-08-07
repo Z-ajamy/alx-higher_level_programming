cc473be35de8b42b0d55c29671c1fbabc8d420ce
Z-ajamy <abdorahman0283@gmail.com>
2025-08-08 01:11:37 +0300

    super is better then Dad LOL

--- 0x0A-python-inheritance/10-main.py
+++ 0x0A-python-inheritance/10-main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 Square = __import__('10-square').Square
 
-s = Square(1340)
-print(s.area())
+try:
+    s = Square("13")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
