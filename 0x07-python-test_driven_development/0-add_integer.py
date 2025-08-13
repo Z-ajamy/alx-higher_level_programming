7332ac74ae8b82f61434ab3e289ec344063dee16
Abd_alrahman Ahmed Mohammed Al_ajamy <140712913+Z-ajamy@users.noreply.github.com>
2025-08-13 07:48:36 +0000

    ؟

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -38,7 +38,4 @@ def add_integer(a, b=98):
         raise TypeError ("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError ("b must be an integer")
-    result = a + b
-    if result == float('inf') or result == -float('inf'):
-        return 89
     return int(a) + int(b)
