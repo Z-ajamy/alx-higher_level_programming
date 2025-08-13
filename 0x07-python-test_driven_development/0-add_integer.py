caa1df843169bb723b71414fe927e1ed71db89ab
Abd_alrahman Ahmed Mohammed Al_ajamy <140712913+Z-ajamy@users.noreply.github.com>
2025-08-13 07:13:39 +0000

    doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -39,7 +39,3 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError ("b must be an integer")
     return int(a) + int(b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
