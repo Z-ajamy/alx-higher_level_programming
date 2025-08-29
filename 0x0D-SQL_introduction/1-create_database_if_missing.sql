e350cf0671313aaa6539676eddde05d5779fc336
Z-ajamy <abdorahman0283@gmail.com>
2025-08-29 20:36:51 +0300

    -- Create a new database named 'hbtn_0c_0' if it doesn't already exist

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Create a new database named 'hbtn_0c_0' if it doesn't already exist
+-- The IF NOT EXISTS clause prevents errors if the database is already present
+-- Backticks around the database name handle any special characters or reserved words
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
