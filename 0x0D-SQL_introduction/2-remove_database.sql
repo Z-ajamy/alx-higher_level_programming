288e4874891f3170f38319d2b755054ea6e818c8
Z-ajamy <abdorahman0283@gmail.com>
2025-08-29 20:38:40 +0300

    -- Delete the database named 'hbtn_0c_0' if it exists

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Delete the database named 'hbtn_0c_0' if it exists
+-- The IF EXISTS clause prevents errors if the database is not present
+-- WARNING: This will permanently delete the database and ALL its data
+-- Backticks around the database name handle any special characters or reserved words
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
