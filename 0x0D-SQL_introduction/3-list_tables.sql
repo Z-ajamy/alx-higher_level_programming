4cce7cecdc5af6867dc3b7a4fd102fb550a36007
Z-ajamy <abdorahman0283@gmail.com>
2025-08-29 20:49:18 +0300

    -- Display all tables in the currently selected database

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- Display all tables in the currently selected database
+-- This command lists table names that the current user has privileges to see
+-- Note: A database must be selected first (using USE database_name) for this to work
+SHOW TABLES;
