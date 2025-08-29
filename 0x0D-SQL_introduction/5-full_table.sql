bbf12fc2f0344f84eb6113d4ce1c420d14bfe0b7
Z-ajamy <abdorahman0283@gmail.com>
2025-08-29 20:55:00 +0300

    -- Display the structure/schema of the 'first_table' table in the 'hbtn_0c_0' database

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
--- Display the structure/schema of the 'first_table' table in the 'hbtn_0c_0' database
+-- Display the structure/schema of the 'first_table' table
 -- This command shows column names, data types, constraints, and other table metadata
 -- Using database.table notation allows querying table structure from any active database
 DESCRIBE hbtn_0c_0.first_table;
