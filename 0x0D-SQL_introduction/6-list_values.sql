35e5c5718cf5376866d09de8c22cb955b4459ed9
Z-ajamy <abdorahman0283@gmail.com>
2025-08-30 16:21:08 +0300

    -- Select all columns and rows from the 'first_table' table in the 'hbtn_0c_0' database

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- Select all columns and rows from the 'first_table' table in the 'hbtn_0c_0' database
+-- Using fully qualified name (database.table) allows querying without switching databases
+-- The asterisk (*) wildcard retrieves all columns: id, name
+-- This will return every record stored in the table
+SELECT * FROM hbtn_0c_0.first_table;
