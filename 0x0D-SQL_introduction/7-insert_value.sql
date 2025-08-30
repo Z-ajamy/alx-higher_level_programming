b0ef084d8bd6dafe50da52754c1e2d4a0fe51057
Z-ajamy <abdorahman0283@gmail.com>
2025-08-30 16:25:07 +0300

    -- Insert a new row into the 'first_table' table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Insert a new row into the 'first_table' table
+-- Values are provided in the same order as table columns: id, name
+-- Using VALUES clause to specify the data for the new record
+INSERT INTO first_table VALUES (89, "Best School");
