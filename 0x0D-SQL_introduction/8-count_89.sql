39273eea6a540914a2385fd8180402dafb9bac15
Z-ajamy <abdorahman0283@gmail.com>
2025-08-30 16:27:22 +0300

    -- Count the number of rows in 'first_table' where the id column equals 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Count the number of rows in 'first_table' where the id column equals 89
+-- This query returns a single number representing how many records match the condition
+-- Useful for checking if a specific ID exists or counting duplicate IDs
+SELECT COUNT(*) FROM first_table WHERE id = 89;
