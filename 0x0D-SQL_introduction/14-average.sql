0cd4d8c756c34d42c5a82f7a02581234f1ab0444
Z-ajamy <abdorahman0283@gmail.com>
2025-08-30 18:16:22 +0300

    -- Calculate the average score from all records in second_table

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- Calculate the average score from all records in second_table
 -- Returns a single decimal value representing the mean of all score values
 -- NULL values in the score column are automatically excluded from the calculation
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) AS `average` FROM second_table;
