dd164459885e562883ed65fe0a3c2361db585585
Z-ajamy <abdorahman0283@gmail.com>
2025-09-03 19:45:42 +0300

    id_not_null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,7 +1,7 @@
--- Create a table named 'force_name' if it doesn't already exist
--- Note: Despite the table name suggesting 'force_name', there's no NOT NULL constraint on name
+-- Create a table named 'id_not_null' if it doesn't already exist
+-- Note: Despite the table name suggesting 'id_not_null', there's no NOT NULL constraint on name
 -- The id column has a default value but can still be NULL if explicitly set
-CREATE TABLE IF NOT EXISTS force_name(
+CREATE TABLE IF NOT EXISTS id_not_null(
     id INT DEFAULT 1,       -- Integer column with default value of 1 (still allows NULL if explicitly set)
     name VARCHAR(256)       -- Variable-length string column (allows NULL, no constraints)
 );
