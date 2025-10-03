bead4ddf00c256e2118472d1af8bdc5ad27ddd22
Z-ajamy <abdorahman0283@gmail.com>
2025-10-03 21:02:41 +0300

    ؟

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
+"""
 
 import urllib.request, urllib.error, sys
 
