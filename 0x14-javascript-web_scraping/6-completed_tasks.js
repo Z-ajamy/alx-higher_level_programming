bb30e1f9ef5701e5b2c716326abe8ffce32b36ce
Z-ajamy <abdorahman0283@gmail.com>
2026-02-02 23:28:47 +0200

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,29 @@
 #!/usr/bin/env node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, { json: true }, function (err, response, body) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    const completedTasks = {};
+
+    for (let i = 0; i < body.length; i++) {
+        const task = body[i];
+
+        if (task.completed === true) {
+            
+            if (!completedTasks[task.userId]) {
+                completedTasks[task.userId] = 1;
+            } else {
+                completedTasks[task.userId] += 1;
+            }
+        }
+    }
+
+    console.log(completedTasks);
+});
