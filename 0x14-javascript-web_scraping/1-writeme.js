2e1cf17e94dae6022a2ed11cca68fe666212729f
Z-ajamy <abdorahman0283@gmail.com>
2026-02-02 16:00:53 +0200

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+function callback (err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+}
+
+fs.writeFile(filePath, content, "utf-8", callback);
