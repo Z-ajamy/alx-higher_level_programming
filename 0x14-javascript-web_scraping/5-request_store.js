b8e816cfa134bccec742a234ab4e87ac719638e5
Z-ajamy <abdorahman0283@gmail.com>
2026-02-02 22:19:18 +0200

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,34 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const request = require('request');
+
+let api = process.argv[2];
+let fpath = process.argv[3];
+
+
+const options = {
+    'url': api,
+};
+
+function callbackReq (err, response, body) {
+    let content;
+
+    if (err) {
+        console.log(err);
+        return null;
+    }
+    content = body;
+
+    if (!content) return null;
+
+    function callbackFile (err) {
+        if (err) {
+            console.log(err);
+            return null;
+        }
+    }
+    fs.writeFile(fpath, content,'UTF-8' , callbackFile);
+
+}
+
+request(options, callbackReq);
