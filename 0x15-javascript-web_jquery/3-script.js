3d9c4fcd5dbd53f462b840674cbf73840e2b28ec
Z-ajamy <abdorahman0283@gmail.com>
2026-02-05 23:21:26 +0200

    Add `.red` class

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('#red_header').click(function() {
-        $('header').addClass('red');
+$(document).ready(function () {
+    $("div#red_header").click(function () {
+        $("header").addClass('red');
     });
 });
