f8697f9492d0ea4602cbae4d1393f135f8ef40f8
Z-ajamy <abdorahman0283@gmail.com>
2026-02-05 23:27:07 +0200

    Toggle classes

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-    $('#toggle_header').click(function() {
-        if ($('header').hasClass('red')) {
-            $('header').removeClass('red').addClass('green');
-        } else {
-            $('header').removeClass('green').addClass('red');
-        }
+    $('DIV#toggle_header').click(function() {
+        ($('header').toggleClass('red green'));
     });
 });
